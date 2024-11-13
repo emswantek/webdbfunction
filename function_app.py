4e22c9f36c59e4d090bad6950696f3e61a44f9f7
Eric Swantek <eric.m.swantek@gmail.com>
2024-11-13 14:17:46 -0500

    Remove name

--- function_app.py
+++ function_app.py
@@ -25,27 +25,12 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-    name = req.params.get('name')
-    
     item = client.read_item(item=row_id)
     item['VisitCount'] += 1
     client.upsert_item(item)
-    
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
 
-    if name:
-        #return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-        return func.HttpResponse(f"Updated number of visitors: {item['VisitCount']} ") 
-
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(
+        f"Updated number of visitors: {item['VisitCount']} ",
+        status_code=200
+    )
         
