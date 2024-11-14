90fb2ce6cfe4571967b3e7223f472eb741628ef3
Eric Swantek <eric.m.swantek@gmail.com>
2024-11-14 11:56:09 -0500

    Revert "Modified DB functionality based on line 233 in SDK"
    This reverts commit af9aad774fd21b4dce5a5d09804ab51a6a7dcbf3.

--- function_app.py
+++ function_app.py
@@ -5,25 +5,20 @@ import uuid
 from azure.cosmos.aio import CosmosClient
 from azure.cosmos import exceptions
 from azure.cosmos.partition_key import PartitionKey
-import  os
 
 endpoint = "https://cloudresbackenddb.documents.azure.com:443/"
 key = "DBKEY"
 
-url = os.environ["DB_ACCOUNT_URI"]
-key = os.environ["DB_ACCOUNT_KEY"]
-client = CosmosClient(url, key)
+database_id = "webstats"
+container_id = "statsonload"
+partition_key = "/id"
+row_id = '1'
 
 # Set the total throughput (RU/s) for the database and container
 database_throughput = 1000
 
 # Singleton CosmosClient instance
-client = CosmosClient(endpoint, key)
-
-# [START get_container]
-database = client.get_database_client(database_name)
-container = database.get_container_client(container_name)
-# [END get_container]
+client = CosmosClient(endpoint, credential=key)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 #Adding a test comment!
@@ -31,11 +26,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-
-    
-    item = container.read_item("VisitCount", partition_key=[1])
+    item = client.read_item(item=row_id)
     item['VisitCount'] += 1
-    updated_item = container.upsert_item(item)
+    CosmosClient.upsert_item(item)
 
     return func.HttpResponse(
         f"Updated number of visitors: {item['VisitCount']} ",
