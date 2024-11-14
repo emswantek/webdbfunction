af9aad774fd21b4dce5a5d09804ab51a6a7dcbf3
Eric Swantek <eric.m.swantek@gmail.com>
2024-11-14 10:19:36 -0500

    Modified DB functionality based on line 233 in SDK

--- function_app.py
+++ function_app.py
@@ -5,20 +5,25 @@ import uuid
 from azure.cosmos.aio import CosmosClient
 from azure.cosmos import exceptions
 from azure.cosmos.partition_key import PartitionKey
+import  os
 
 endpoint = "https://cloudresbackenddb.documents.azure.com:443/"
 key = "DBKEY"
 
-database_id = "webstats"
-container_id = "statsonload"
-partition_key = "/id"
-row_id = '1'
+url = os.environ["DB_ACCOUNT_URI"]
+key = os.environ["DB_ACCOUNT_KEY"]
+client = CosmosClient(url, key)
 
 # Set the total throughput (RU/s) for the database and container
 database_throughput = 1000
 
 # Singleton CosmosClient instance
-client = CosmosClient(endpoint, credential=key)
+client = CosmosClient(endpoint, key)
+
+# [START get_container]
+database = client.get_database_client(database_name)
+container = database.get_container_client(container_name)
+# [END get_container]
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 #Adding a test comment!
@@ -26,9 +31,11 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-    item = client.read_item(item=row_id)
+
+    
+    item = container.read_item("VisitCount", partition_key=[1])
     item['VisitCount'] += 1
-    CosmosClient.upsert_item(item)
+    updated_item = container.upsert_item(item)
 
     return func.HttpResponse(
         f"Updated number of visitors: {item['VisitCount']} ",
