791047979b255e4c59b44af291ec2e0c92d338f0
Eric Swantek <eric.m.swantek@gmail.com>
2024-11-15 15:20:04 -0500

    Overhauled code for real this  time

--- function_app.py
+++ function_app.py
@@ -3,51 +3,47 @@ import logging
 from azure.cosmos.aio import CosmosClient
 import os
 
-#database_id = "webstats"
-#container_id = "statsonload"
+database_name = "webcounter"
+container_name = "viewtracker"
 
-#original DB (lines 10 - 11)
-database_name = "webstats"
-container_name = "NumberOfWebViews"
-#New DB (lines 13 - 14)
-#database_name = "SampleDB"
-#container_name = "SampleContainer"
+url = os.environ.get("DB_ACCOUNT_URL")
+key = os.environ.get("DB_ACCOUNT_KEY")
 
-#partition_key = "1"
-#row_id = '1'
-
-url = os.environ["DB_ACCOUNT_URL"]
-key = os.environ["DB_ACCOUNT_KEY"]
-
+if not url or not key:
+    raise ValueError("DB_ACCOUNT_URL and DB_ACCOUNT_KEY must be set in the environment variables.")
 
 client = CosmosClient(url, key)
-#client = CosmosClient(url, credential=key)
-
-# Set the total throughput (RU/s) for the database and container
-# database_throughput = 1000
-
 database = client.get_database_client(database_name)
 container = database.get_container_client(container_name)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="http_trigger2")
-async def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('1: Python HTTP trigger function processed a request.')
-    
-    item = await container.read_item("1", partition_key="/id")
-    #item = client.read_item(item=row_id)
-    #Alternate attempts commented out
-    #item = await container.read_item("1", partition_key="75BF1ACB-168D-469C-9AA3-1FD26BB4EA4C")
-    #item = container.read_item("VisitCount")
-    logging.info("2: hello")
-    logging.info(item)
-    item += 1
 
-    updated_item = await container.upsert_item(item)
+@app.route(route="http_trigger2")
+async def http_trigger2() -> func.HttpResponse:
+    logging.info('1: Python HTTP trigger function processed a request.')
+
+    item_id = "1"
+    partition_key_value = "visitorCount"
+
+    try:
+        # Read the item from the container
+        item = await container.read_item(item=item_id, partition_key=partition_key_value)
+        logging.info(f"2: {item}")
+
+        # Increment the visitor count
+        item['count'] += 1
+
+        # Upsert the item back into the container
+        updated_item = await container.upsert_item(item)
+    except Exception as e:
+        logging.error(f"Error processing the request: {str(e)}")
+        return func.HttpResponse(
+            "An error occurred while processing your request.",
+            status_code=500
+        )
 
     return func.HttpResponse(
-        f"Updated number of visitors: {updated_item} ",
+        f"Updated number of visitors: {updated_item['count']}",
         status_code=200
     )
-        
