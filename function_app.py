cea89aa4edca8d0043a0f0e707829ad89abd3162
Eric Swantek <eric.m.swantek@gmail.com>
2024-11-15 16:18:47 -0500

    idk my bff jenny

--- function_app.py
+++ function_app.py
@@ -3,16 +3,29 @@ import logging
 from azure.cosmos.aio import CosmosClient
 import os
 
+#database_id = "webstats"
+#container_id = "statsonload"
+
+#original DB (lines 10 - 11)
+#database_name = "webstats"
+#container_name = "NumberOfWebViews"
+#New DB (lines 13 - 14)
 database_name = "webcounter"
 container_name = "viewtracker"
 
-item_id = "1"
-partition_key_value = "visitorCount"
+#partition_key = "1"
+#row_id = '1'
 
 url = os.environ["DB_ACCOUNT_URL"]
 key = os.environ["DB_ACCOUNT_KEY"]
 
+
 client = CosmosClient(url, key)
+#client = CosmosClient(url, credential=key)
+
+# Set the total throughput (RU/s) for the database and container
+# database_throughput = 1000
+
 database = client.get_database_client(database_name)
 container = database.get_container_client(container_name)
 
@@ -20,16 +33,19 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger2")
 async def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("1: Python HTTP trigger function processed a request.")
-
-    item = await container.read_item(item_id, partition_key=partition_key_value)
-    logging.info(f"2: {item}")
-
-    item["count"] += 1
+    logging.info('1: Python HTTP trigger function processed a request.')
+    
+    #item = client.read_item(item=row_id)
+    item = await container.read_item("1", partition_key="rownum")
+    #item = container.read_item("VisitCount")
+    logging.info("2: hello")
+    logging.info(item)
+    item += 1
 
     updated_item = await container.upsert_item(item)
 
     return func.HttpResponse(
-        f"Updated number of visitors: {updated_item["count"]}",
+        f"Updated number of visitors: {updated_item} ",
         status_code=200
     )
+      
