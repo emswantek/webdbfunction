08caebe5c9373b3c8387b6c977f54f806544cea3
Eric Swantek <eric.m.swantek@gmail.com>
2024-11-15 16:14:53 -0500

    idk , quotes???

--- function_app.py
+++ function_app.py
@@ -3,14 +3,14 @@ import logging
 from azure.cosmos.aio import CosmosClient
 import os
 
-database_name = 'webcounter'
-container_name = 'viewtracker'
+database_name = "webcounter"
+container_name = "viewtracker"
 
-item_id = '1'
-partition_key_value = 'visitorCount'
+item_id = "1"
+partition_key_value = "visitorCount"
 
-url = os.environ['DB_ACCOUNT_URL']
-key = os.environ['DB_ACCOUNT_KEY']
+url = os.environ["DB_ACCOUNT_URL"]
+key = os.environ["DB_ACCOUNT_KEY"]
 
 client = CosmosClient(url, key)
 database = client.get_database_client(database_name)
@@ -18,22 +18,18 @@ container = database.get_container_client(container_name)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-
-@app.route(route='http_trigger2')
+@app.route(route="http_trigger2")
 async def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('1: Python HTTP trigger function processed a request.')
+    logging.info("1: Python HTTP trigger function processed a request.")
 
-    # Read the item from the container
     item = await container.read_item(item_id, partition_key=partition_key_value)
-    logging.info(f'2: {item}')
+    logging.info(f"2: {item}")
 
-    # Increment the visitor count
-    item['count'] += 1
+    item["count"] += 1
 
-    # Upsert the item back into the container
     updated_item = await container.upsert_item(item)
 
     return func.HttpResponse(
-        f'Updated number of visitors: {updated_item['count']}',
+        f"Updated number of visitors: {updated_item["count"]}",
         status_code=200
     )
